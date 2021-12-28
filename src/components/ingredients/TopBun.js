aaceb0f7ec45b6e92a49ae7d35d198920755c923
Max Oppor <max.oppor1@gmail.com>
2021-12-28 10:24:09 -0600

    pre-made components

---
+++ src/components/ingredients/TopBun.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function TopBun() {
+    return (
+        <div className="TopBun">
+            <div className="Seeds1"></div>
+            <div className="Seeds2"></div>
+        </div>
+    );
+}
